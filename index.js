ba5ae67bdf2ebb70ddb779853f1501d5f4ce1273
vishal <vishal.umrao.25.1@mountblue.tech>
2023-10-21 10:39:19 +0530

    cors added

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+
+const cors = require("cors"); // Import the 'cors' package
+
+app.use(cors());
 
 app.use(express.json());
 
 const tasks = [];
 
-app.get('/tasks', (req, res) => {
+app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
-app.post('/tasks', (req, res) => {
+app.post("/tasks", (req, res) => {
   const task = req.body;
   tasks.push(task);
   res.json(task);
 });
 
-app.put('/tasks/:id', (req, res) => {
+app.put("/tasks/:id", (req, res) => {
   const id = req.params.id;
   const updatedTask = req.body;
   tasks[id] = updatedTask;
   res.json(updatedTask);
 });
 
-app.delete('/tasks/:id', (req, res) => {
+app.delete("/tasks/:id", (req, res) => {
   const id = req.params.id;
   tasks.splice(id, 1);
-  res.json({ message: 'Task deleted' });
+  res.json({ message: "Task deleted" });
 });
 
 app.listen(port, () => {
